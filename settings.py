0806f59cd7f85c2fc9de29261f66ff10c4d98d72
Dandekar <anay.dandekar29@bcp.org>
2026-02-13 09:28:30 -0800

    Added slight hitbox reduction for player for better movement

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import pygame as pg
 
-# game variables
+# ----- game variables -----
 
 WIDTH = 800
 HEIGHT = 600
@@ -8,12 +8,16 @@ TITLE = "First Game"
 FPS = 60
 TILESIZE = 32
 
-# player values
+# ----- player values -----
 
+# this value is removed from the player's hitbox size
+# this allows it to move through small gaps easily
+PLAYER_HITBOX_CLEARANCE = 5
 PLAYER_SPEED = 500
-PLAYER_HIT_RECT = pg.Rect(0, 0, TILESIZE, TILESIZE)
+PLAYER_HIT_RECT = pg.Rect(0, 0, TILESIZE - PLAYER_HITBOX_CLEARANCE, TILESIZE - PLAYER_HITBOX_CLEARANCE)
 
-# color values
+# ----- color values -----
+
 # colors are stored as tuples with RGB values
 
 BLUE = (0, 0, 255)
